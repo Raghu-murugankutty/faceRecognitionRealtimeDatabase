3df999e0cfdece3530535c0e7a12fe076f72d25e
Raghu Murugankutty <41443395+Raghu-murugankutty@users.noreply.github.com>
2023-10-20 15:03:19 +0100

    v2

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -3,6 +3,17 @@ import face_recognition
 import pickle
 import os
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://faceattendancerealtime-790e0-default-rtdb.firebaseio.com/',
+    'storageBucket': 'faceattendancerealtime-790e0.appspot.com'
+})
+
 
 folderPath = 'Images'
 pathList = os.listdir(folderPath)
@@ -14,6 +25,11 @@ for path in pathList:
     # print(os.path.splitext(path)[0])
     studentIds.append(os.path.splitext(path)[0])
     print(studentIds)
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 # print(len(imgList))
 
 def findEncodings(imagesList):
